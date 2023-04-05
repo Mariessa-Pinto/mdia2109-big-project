574bed556f663ea236af18e59144aca78faa6a7e
bguemos <bri-lynn@hotmail.com>
2023-04-05 15:31:15 -0700

    pages and css

---
+++ components/Footer/index.js
@@ -0,0 +1,36 @@
+
+
+export default function Footer() {
+
+
+    return(
+        <>
+         <footer>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/resources">
+              <a>Resources</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/landingpage">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/stories">
+              <a>Stories</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/settings">
+              <a>Settings</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+   </>
+    )
+}

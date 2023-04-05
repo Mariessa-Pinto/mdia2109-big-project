ba8d4224212dbb0da896cacc4fa15e696589ef11
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-04-05 07:58:46 -0700

    worked on adding layout to q1

--- pages/q1.js
+++ pages/q1.js
@@ -7,22 +7,24 @@ export default function Q1() {
         <div className={styles.main}>
             <div className={styles.titleSection}>
                 <h1>Demographic Questionnaire</h1>
-
+                <div className={styles.progressBar}>
+                    <div className={styles.progress}></div>
+                </div>
                 <p>1/5</p>
             </div>
             <div className={styles.questionSection}>
                 <h2 className={styles.question}>How old are you?</h2>
                 <div className={styles.answerBox}>
-                    <button className={styles.answerButton}></button>
+                    <button className={styles.answerButton}>Under 18</button>
                 </div>
                 <div className={styles.answerBox}>
-                    <button className={styles.answerButton}></button>
+                    <button className={styles.answerButton}>19-25</button>
                 </div>
                 <div className={styles.answerBox}>
-                    <button className={styles.answerButton}></button>
+                    <button className={styles.answerButton}>26-40</button>
                 </div>
                 <div className={styles.answerBox}>
-                    <button className={styles.answerButton}></button>
+                    <button className={styles.answerButton}>40+</button>
                 </div>
                 <BackButton/>
             </div>

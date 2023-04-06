d608c10ff8ec96512be1872f8695b57ed2ee2ec4
bguemos <bri-lynn@hotmail.com>
2023-04-05 17:29:21 -0700

    footer template

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -5,32 +5,7 @@ export default function Footer() {
 
     return(
         <>
-         <footer>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/resources">
-              <a>Resources</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/landingpage">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/stories">
-              <a>Stories</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/settings">
-              <a>Settings</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </footer>
+        
    </>
     )
 }

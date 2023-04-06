73eee3337f4c139aa70b9f13207a2086262d4364
bguemos <bri-lynn@hotmail.com>
2023-04-06 10:02:42 -0700

    default templates

---
+++ pages/landingpage3.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+import styles from '@/landingpage3.module.css'
+
+
+export default function Landingpage3() {
+
+
+
+    return(
+        <>
+        
+        
+        </>
+    )
+}
---
+++ pages/landingpage4.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+import styles from '@/landingpage4.module.css'
+
+
+export default function LandingPage4() {
+
+
+
+
+    return(
+        <>
+        
+        
+        </>
+    )
+}
+

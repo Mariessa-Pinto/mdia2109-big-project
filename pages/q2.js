be94522e3406821ea78b5a4dd0b383d866ad8c69
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-04-08 15:04:54 -0700

    q2 clicking functionality set up

--- pages/q2.js
+++ pages/q2.js
@@ -7,6 +7,15 @@ export default function Q2() {
 
     const [clicked, setClicked] = useState(false);
 
+    const [fClicked, setFClicked] = useState(false);
+    const femaleClassName = fClicked ? `${styles["answerButtonActive"]} ${styles.answerButton}` : styles.answerButton;
+    const [mClicked, setMClicked] = useState(false);
+    const maleClassName = mClicked ? `${styles["answerButtonActive"]} ${styles.answerButton}` : styles.answerButton;
+    const [oClicked, setOClicked] = useState(false);
+    const otherClassName = oClicked ? `${styles["answerButtonActive"]} ${styles.answerButton}` : styles.answerButton;
+    const [nClicked, setNClicked] = useState(false);
+    const notClassName = nClicked ? `${styles["answerButtonActive"]} ${styles.answerButton}` : styles.answerButton;
+
     return (
         <>
         <div className={styles.main}>
@@ -21,12 +30,17 @@ export default function Q2() {
             </div>
             <div className={styles.questionSection}>
                 <h2 className={styles.question}>What is your gender?</h2>
-                    <button className={styles.answerButton}>Female</button>
-                    <button className={styles.answerButton}>Male</button>
-                    <button className={styles.answerButton}>Other</button>
-                    <button className={styles.answerButton}>Prefer to not answer</button>
+                    <button onClick={() => {setClicked(!clicked); setFClicked(!fClicked)}} className={femaleClassName}>Female</button>
+                    <button onClick={() => {setClicked(!clicked); setMClicked(!mClicked)}} className={maleClassName}>Male</button>
+                    <button onClick={() => {setClicked(!clicked); setOClicked(!oClicked)}} className={otherClassName}>Other</button>
+                    <button onClick={() => {setClicked(!clicked); setNClicked(!nClicked)}} className={notClassName}>Prefer to not answer</button>
             </div>
-            <Link href={'./q1'}><BackButton/></Link>
+            <div className={styles.controls}>
+                <Link href={'./q1'}><BackButton/></Link>
+                {
+                    clicked ? <><Link href={'./q3'}><button className={styles.nextButton}>Next</button></Link></> : <></>
+                }
+            </div>
         </div>
         </>
     )

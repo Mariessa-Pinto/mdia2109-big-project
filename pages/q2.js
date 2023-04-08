c2c2906b8b1ce2b3721e343683573f58fb959c3a
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-04-07 17:47:08 -0700

    added q2 content and stylizing

--- pages/q2.js
+++ pages/q2.js
@@ -1,31 +1,32 @@
 import styles from '@/styles/Q2.module.css'
 import BackButton from '@/components/BackButton'
+import { useState } from 'react';
+import Link from 'next/link';
 
 export default function Q2() {
+
+    const [clicked, setClicked] = useState(false);
+
     return (
         <>
         <div className={styles.main}>
             <div className={styles.titleSection}>
-                <h1>Demographic Questionnaire</h1>
-
-                <p>2/5</p>
+                <h1 className={styles.header}>Getting to know you</h1>
+                <div className={styles.progressBar}>
+                    <div className={styles.progress}></div>
+                </div>
+                <div className={styles.questionNumber}>
+                    <p>2/5</p>
+                </div>
             </div>
             <div className={styles.questionSection}>
                 <h2 className={styles.question}>What is your gender?</h2>
-                <div className={styles.answerBox}>
-                    <button className={styles.answerButton}></button>
-                </div>
-                <div className={styles.answerBox}>
-                    <button className={styles.answerButton}></button>
-                </div>
-                <div className={styles.answerBox}>
-                    <button className={styles.answerButton}></button>
-                </div>
-                <div className={styles.answerBox}>
-                    <button className={styles.answerButton}></button>
-                </div>
-                <BackButton/>
+                    <button className={styles.answerButton}>Female</button>
+                    <button className={styles.answerButton}>Male</button>
+                    <button className={styles.answerButton}>Other</button>
+                    <button className={styles.answerButton}>Prefer to not answer</button>
             </div>
+            <Link href={'./q1'}><BackButton/></Link>
         </div>
         </>
     )

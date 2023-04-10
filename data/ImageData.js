abd39875457566af7ec58f289aa717d17893e694
bguemos <bri-lynn@hotmail.com>
2023-04-10 13:07:36 -0700

    data file and carousel

---
+++ data/ImageData.js
@@ -0,0 +1,18 @@
+export const photos = [
+    {
+        id: 0, 
+        Image: '/carousel-images/0.png',
+    
+    },
+    {
+        id: 1,
+        Image: '/carousel-images/1.png',
+    
+    },
+    {
+        id: 2, 
+        Image: '/carousel-images/2.png',
+        
+    }
+    
+    ]

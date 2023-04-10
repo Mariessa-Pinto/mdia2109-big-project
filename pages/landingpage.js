8c6a5361c065d590e6607ee533e6692d83d4aa02
bguemos <bri-lynn@hotmail.com>
2023-04-09 23:55:01 -0700

    start of landing page

--- pages/landingpage.js
+++ pages/landingpage.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '@/styles/landingpage.module.css'
+import styles from '@/styles/LandingPage.module.css'
 import Footer from '@/components/Footer'
 
 export default function LandingPage() {
@@ -8,7 +8,23 @@ export default function LandingPage() {
     return( 
 <main className={styles.main}>
 
-<div>
+
+
+
+<div className={styles.top}>
+     <img className={styles.logo} src='./logo/smalllogo.svg'></img>
+</div>
+<div className={styles.title}>
+     <h1 className={styles.header}>Curated For You</h1>
+
+ </div>
+
+  
+
+
+<div className={styles.stories}>
+    <img className={styles.pencil} src='./icons/dashboardstories.png'></img>
+    <a href="stories">Happy News From Home</a>
 
 
 </div>
@@ -16,6 +32,7 @@ export default function LandingPage() {
 
 
 <div>
+
 <Footer/>
 
 

271da091d1353158ebc72624e3c52f541a597d5a
bguemos <bri-lynn@hotmail.com>
2023-04-13 10:56:37 -0700

    data file for stories

---
+++ pages/s2.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '@/styles/s2.module.css'
+import Footer from '@/components/Footer'
+
+
+export default function StoryTwo() {
+
+    return ( 
+
+        <main className={styles.main}>
+
+        <div>
+          
+          
+        </div>
+
+
+    </main>
+    )
+}

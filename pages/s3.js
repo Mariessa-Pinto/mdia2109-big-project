831404a91a7ad1ff2ef8603a14e2a63c883066b0
bguemos <bri-lynn@hotmail.com>
2023-04-14 09:25:29 -0700

    modifed story templates

--- pages/s3.js
+++ pages/s3.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/s3.module.css'
 import Footer from '@/components/Footer'
+import { storyData } from '@/data/organizationdata'
 
 
 export default function StoryThree() {

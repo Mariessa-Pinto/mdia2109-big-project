78b88fc7b1416ae73780374ccbe2d414476f467e
bguemos <bri-lynn@hotmail.com>
2023-04-16 11:10:47 -0700

    landing page css

--- components/FemaleChart/index.js
+++ components/FemaleChart/index.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import styles from './doughnut.module.css'
+import styles from './chart3.module.css'
 import React from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';

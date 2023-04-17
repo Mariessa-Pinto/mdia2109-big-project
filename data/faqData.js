7454c9fc1fa7bb7fac61dde6b748bf64f5bad104
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-04-17 12:12:28 -0700

    added faq data

---
+++ data/faqData.js
@@ -0,0 +1,32 @@
+export const faqData = {
+    questions: [
+        {
+            question: "What is BorderLess?",
+            answer: "BorderLess is a web application designed for refugees in Canada. As a refugee, BorderLess offers access to stories of Hope and Help, weather from your home region and most importantly resources. The resources available are regularly updated and specific to the province or territory the refugee chooses."
+        },
+        {
+            question: "I have resources to share",
+            answer: "Wonderful! We always appreciate any help. Please contact us at borderless@gmail.com to get started. We are always looking to partner for people or organizations who have food, shelter, communities, clothing or other donations to help local refugees."
+        },
+        {
+            question: "How do I contact BorderLess staff?",
+            answer: "Staff and volunteers can be reached through our email at borderless@gmail.com. We try our best to respond to emails within 24hours. If you have not heard back from us, please send a follow-up email. We will get back to you as soon as possible."
+        },
+        {
+            question: "What are Stories of Hope and Help?",
+            answer: "Stories of Hope and Help are positive stories collected from agencies from around the world. The focus of the stories is on refugee stories that spread positive news and positive experiences in order to bring hope to those navigating difficult waters."
+        },
+        {
+            question: "How do I access resources?",
+            answer: "BorderLess aims to give easy access to all resources listed on the site. Simply reach out to the contact listed to learn how to receive the resource. If there is an issue receiving a resource from a listed partner, please reach out to us regarding the issue for us to help you out."
+        },
+        {
+            question: "How can I help?",
+            answer: "If you would like to volunteer or help out please contact us at borderless@gmail.com. We are always looking for volunteers to help run and improve services offered. If you have donations, please refer to the “I have resources to share” FAQ."
+        },
+        {
+            question: "Why is there a quiz?",
+            answer: "We take privacy concerns very seriously. The quiz is purposed for us to get to know you a bit. This helps us filter the large amount of resources available across the nation to better fit you. It also helps us gather insights on who needs this app most in order to provide more of the right resources to help."
+        },
+    ]
+}

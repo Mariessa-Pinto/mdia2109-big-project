4f164be880928af724a0a5ef319921e573a4304d
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-04-17 12:11:45 -0700

    made faq page

--- pages/faq.js
+++ pages/faq.js
@@ -1,6 +1,37 @@
+import styles from '@/styles/Faq.module.css'
+import FAQAccordion from '@/components/FAQAccordion'
+import Footer from '@/components/Footer'
+import BackButton from '@/components/BackButton/index.js'
+import { faqData } from '../data/faqData.js'
+import { useState, useEffect } from 'react'
+import Link from 'next/link.js'
+
 export default function FAQ() {
+
+    const [data, setData] = useState([...faqData.questions]);
+
     return (
         <>
+        <div className={styles.main}>
+            <div className={styles.title}>
+                <h1 className={styles.header}>FAQ</h1>
+                <div className={styles.back}>
+                    <Link href={'/settings'}><BackButton/></Link>
+                </div>
+            </div>
+            <div>
+            {
+            data && data.map((info, index) => {
+                return(
+                <div className={styles.questions}>
+                    <FAQAccordion key={index} question={info.question} answer={info.answer}/>
+                </div>
+                )
+            })
+            }
+            </div>
+        </div>
+        <Footer/>
         </>
     )
 }

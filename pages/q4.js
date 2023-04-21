2c2b32d9a8d55a4aef91bfc1cebf9d896dddffa2
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-04-21 11:23:27 -0700

    Made responsive

--- pages/q4.js
+++ pages/q4.js
@@ -10,49 +10,49 @@ export default function Q4() {
     const [value, setValue] = useState('');
 
     return (
-        <>
-        <div className={styles.main}>
-            <div className={styles.titleSection}>
-                <div className={styles.skip}>
-                    <Link href={'/landingpage'}><SkipButton/></Link>
-                </div>
-                <h1 className={styles.header}>Getting to know you</h1>
-                <div className={styles.progressBar}>
-                    <div className={styles.progress}></div>
-                </div>
-                <div className={styles.questionNumber}>
-                    <p>4/5</p>
-                </div>
-            </div>
-            <div className={styles.questionSection}>
-                <h2 className={styles.question}>How satisfied are you with your experience as a refugee in Canada?</h2>
-                <div className={styles.answerBox}>
-                    <div className={styles.rangeSlider}>
-                        <div className={styles.sliderLabels}>
-                            <p>Not Satisfied</p>
-                            <p>Satisfied</p>
-                            <p>Very Satisfied</p>
-                        </div>
-                        <input 
-                            type='range' 
-                            min={1} 
-                            max={100} 
-                            value={value} 
-                            className={styles.mySlider}
-                            onChange={(event) => setValue(event.target.value)}
-                        />
-                        <p className={styles.value}>Current Value: {value}</p>
+        <div className={styles.container}>
+            <div className={styles.main}>
+                <div className={styles.titleSection}>
+                    <div className={styles.skip}>
+                        <Link href={'/landingpage'}><SkipButton/></Link>
+                    </div>
+                    <h1 className={styles.header}>Getting to know you</h1>
+                    <div className={styles.progressBar}>
+                        <div className={styles.progress}></div>
+                    </div>
+                    <div className={styles.questionNumber}>
+                        <p>4/5</p>
                     </div>
                 </div>
-            </div>
-            <div className={styles.seperator}></div>
-            <div className={styles.controls}>
-                <Link href={'./q3'}><BackButton/></Link>
-                {
-                    value !== '' ? <><Link href={'./q5'}><button className={styles.nextButton}>Next</button></Link></> : <></>
-                }
+                <div className={styles.questionSection}>
+                    <h2 className={styles.question}>How satisfied are you with your experience as a refugee in Canada?</h2>
+                    <div className={styles.answerBox}>
+                        <div className={styles.rangeSlider}>
+                            <div className={styles.sliderLabels}>
+                                <p>Not Satisfied</p>
+                                <p>Satisfied</p>
+                                <p>Very Satisfied</p>
+                            </div>
+                            <input 
+                                type='range' 
+                                min={1} 
+                                max={100} 
+                                value={value} 
+                                className={styles.mySlider}
+                                onChange={(event) => setValue(event.target.value)}
+                            />
+                            <p className={styles.value}>Current Value: {value}</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.seperator}></div>
+                <div className={styles.controls}>
+                    <Link href={'./q3'}><BackButton/></Link>
+                    {
+                        value !== '' ? <><Link href={'./q5'}><button className={styles.nextButton}>Next</button></Link></> : <></>
+                    }
+                </div>
             </div>
         </div>
-        </>
     )
 }

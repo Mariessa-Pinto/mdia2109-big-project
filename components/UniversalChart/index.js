381a6873725492dc3d6b8efec82dab54d3fc46cd
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-04-28 10:35:09 -0700

    updated font sizing on charts

--- components/UniversalChart/index.js
+++ components/UniversalChart/index.js
@@ -1,12 +1,13 @@
-import { useState, useEffect } from "react"
 import styles from './UniversalChart.module.css'
 import React from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
 
-ChartJS.register(ArcElement, Tooltip, Legend);
-
-
+ChartJS.register(
+  ArcElement, 
+  Tooltip, 
+  Legend
+);
 
 export const data = {
     labels: ['Canadian Born', 'Refugees'],
@@ -17,38 +18,25 @@ export const data = {
         backgroundColor: [
           'rgba(75, 192, 100, 0.2)',
           'rgba(54, 162, 235, 0.2)',
-        
-         
         ],
         borderColor: [
           'rgba(75, 192, 100, 0.9)',
           'rgba(54, 162, 235, 1)',
-         
-        
         ],
         borderWidth: 1,
       },
     ],
   };
   
-  export default function UniversalChart() {
-    return (
-      <>
-      <h1 className={styles.heading}> Home Ownership </h1>
+export default function UniversalChart() {
+  return (
+    <>
+      <h1 className={styles.heading}>Home Ownership</h1>
       <p className={styles.describer}>Owning a home is not only a symbol of financial stability but also represents a family's investment in a community. Despite facing initial financial challenges, a significant number of refugee families in Canada have been able to achieve this milestone</p>
       <div className={styles.doughnut}>
-        
         <Doughnut data={data}/>
-    </div>
-         
-  <div className={styles.description}>
-  
-  <h2 className={styles.describer}>Canadian Refugees</h2>
-  <h2 className={styles.describer}>Canadian Born</h2>
-  </div>
-      </>
-    )
-   
-  
-  }
+      </div>       
+    </>
+  )
+}
   
